33b445111e09b8839c3fb31c461ce649032d7dfd
Ilya Skumin <ilya_skumin@epam.com>
2023-01-17 11:20:31 +0300

    Initial commit

---
+++ src/test/java/WeightTest.java
@@ -0,0 +1,186 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class WeightTest {
+    @Test
+    @DisplayName("Test number #1")
+    void falseTest1() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #2")
+    void falseTest2() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #3")
+    void falseTest3() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #4")
+    void falseTest4() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #5")
+    void falseTest5() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #6")
+    void falseTest6() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #7")
+    void falseTest7() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #8")
+    void falseTest8() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #9")
+    void falseTest9() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #10")
+    void falseTest10() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #11")
+    void falseTest11() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #12")
+    void falseTest12() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #13")
+    void falseTest13() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #14")
+    void falseTest14() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #15")
+    void falseTest15() {
+        assertEquals(5 + 1, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #16")
+    void trueTest16() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #17")
+    void trueTest17() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #18")
+    void trueTest18() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #19")
+    void trueTest19() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #20")
+    void trueTest20() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #21")
+    void trueTest21() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #22")
+    void trueTest22() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #23")
+    void trueTest23() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #24")
+    void trueTest24() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #25")
+    void trueTest25() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #26")
+    void trueTest26() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #27")
+    void trueTest27() {
+        assertEquals(5 + 2, 7);
+    }
+
+    @Test
+    @DisplayName("Test number #28")
+    void trueTest28() {
+        assertEquals(5 + 2, 7);
+    }
+    @Test
+    @DisplayName("Test number #29")
+    void trueTest29() {
+        assertEquals(5 + 2, 7);
+    }
+    @Test
+    @DisplayName("Test number #30")
+    void trueTest30() {
+        assertEquals(5 + 2, 7);
+    }
+}
